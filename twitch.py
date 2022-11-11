377d3843d7d4a20e690dc1d9e599d7944617a7b4
Eddie-254 <nderituedwin90@gmail.com>
2022-11-11 17:00:09 +0300

    Initial

--- twitch.py
+++ twitch.py
@@ -10,28 +10,34 @@ import os
 #import wget
 os.environ['PATH'] += r"C:/Chromedriver"
 
-options = uc.ChromeOptions()
-options.add_argument("user-data-dir=C:\\Users\\user\\AppData\\Local\\Google\\Chrome\\User Data")
-options.add_argument("Profile 2")
-driver = uc.Chrome(chrome_options=options, use_subprocess=True)
-driver.set_window_size(1024, 600)
-driver.maximize_window()
+def twitch_login():
+    options = uc.ChromeOptions()
+    options.add_argument("user-data-dir=C:\\Users\\user\\AppData\\Local\\Google\\Chrome\\User Data")
+    options.add_argument("Profile 1")
+    options.add_experimental_option("detach", True)
+    driver = uc.Chrome(chrome_options=options, use_subprocess=True)
+    driver.set_window_size(1024, 600)
+    driver.maximize_window()
 
 
-#open the webpage
-driver.get("https://www.twitch.tv/brawlhalla")
+    #open the webpage
+    driver.get("https://www.twitch.tv/brawlhalla")
 
-#target username
-login = WebDriverWait(driver, 20).until(EC.element_to_be_clickable((By.CSS_SELECTOR, "#root > div > div.Layout-sc-nxg1ff-0.bSuLAT > nav > div > div.Layout-sc-nxg1ff-0.hWJFll > div.Layout-sc-nxg1ff-0.jwASrz > div > div.Layout-sc-nxg1ff-0.iLhppl.anon-user > div:nth-child(1) > button > div > div"))).click()
-username = WebDriverWait(driver, 10).until(EC.element_to_be_clickable((By.CSS_SELECTOR, "#login-username")))
-password = WebDriverWait(driver, 10).until(EC.element_to_be_clickable((By.CSS_SELECTOR, "#password-input")))
+    #target username
+    login = WebDriverWait(driver, 20).until(EC.element_to_be_clickable((By.CSS_SELECTOR, "#root > div > div.Layout-sc-nxg1ff-0.bSuLAT > nav > div > div.Layout-sc-nxg1ff-0.hWJFll > div.Layout-sc-nxg1ff-0.jwASrz > div > div.Layout-sc-nxg1ff-0.iLhppl.anon-user > div:nth-child(1) > button > div > div"))).click()
+    username = WebDriverWait(driver, 10).until(EC.element_to_be_clickable((By.CSS_SELECTOR, "#login-username")))
+    password = WebDriverWait(driver, 10).until(EC.element_to_be_clickable((By.CSS_SELECTOR, "#password-input")))
 
-#enter username and password
-username.clear()
-username.send_keys("3ddi6")
-password.clear()
-password.send_keys("vC%=xA!i&Z7bgUT")
+    #enter username and password
+    username.clear()
+    username.send_keys("3ddi6")
+    password.clear()
+    password.send_keys("vC%=xA!i&Z7bgUT")
 
-#target the login button and click it
-button = WebDriverWait(driver, 60).until(EC.element_to_be_clickable((By.CSS_SELECTOR, "body > div.ReactModalPortal > div > div > div > div > div > div.Layout-sc-nxg1ff-0.gmqBFP > div > div > div.Layout-sc-nxg1ff-0.gZaqky > form > div > div:nth-child(3) > button > div > div"))).click()
+    #target the login button and click it
+    button = WebDriverWait(driver, 60).until(EC.element_to_be_clickable((By.CSS_SELECTOR, "body > div.ReactModalPortal > div > div > div > div > div > div.Layout-sc-nxg1ff-0.gmqBFP > div > div > div.Layout-sc-nxg1ff-0.gZaqky > form > div > div:nth-child(3) > button > div > div"))).click()
 
+    while(True):
+        pass
+
+twitch_login()    
